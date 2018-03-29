dec4ef41ccbd2e9bf5d8e8800dc1cf7201a2520c
Digital Corps <digitalcorps@bsu.edu>
2018-03-28 22:12:14 -0400

    initial commit

---
+++ Assets/Spawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn: MonoBehaviour {
+
+	private SteamVR_TrackedObject trackedObj;
+	private GameObject objectInHand;
+	private SteamVR_Controller.Device Controller {
+		get { return SteamVR_Controller.Input ((int)trackedObj.index); }
+	}
+
+	public GameObject hillPrefab;
+
+
+	void Awake() {
+		trackedObj = GetComponent<SteamVR_TrackedObject> ();
+	}
+
+	void SpawnHill() {
+		GameObject hill = Instantiate (hillPrefab);
+		hill.SetActive(true);
+		hill.transform.position = gameObject.transform.position;
+	}
+	
+	void Update () {
+		if (Controller.GetHairTriggerDown ()) {
+			SpawnHill();
+		}
+	}
+}
